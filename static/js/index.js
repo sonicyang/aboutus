119261462be41629ee92bd236fcf0fc857970295
alanaudi <alan.audi.micro@hotmail.com>
2015-10-19 15:22:46 +0800

    alanaudi

--- static/js/index.js
+++ static/js/index.js
@@ -29,3 +29,14 @@ $('nav a').on('click', function(){
     $("nav li").attr("class", "");
     $(this).parent().attr("class", "active");
 })
+
+// put a dark layer on the header image 
+$(function(){
+    // 預設的透明度值
+    var _opacity = 0.4;
+    // 在 .mask-div 中加入一個遮罩用的 .cover-mask, 並設定其透明度為預設的透明度值
+    // 接著加入 .hover() 事件
+    $('.mask-div').append('<div class="cover-mask"></div>').find('.cover-mask').css({
+        opacity: _opacity
+    });
+});
